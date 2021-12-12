50afdbcd71081faa3c8221add38ad1ecbc5a6a1b
solovyovsdom <joelborofsky@mac.com>
2021-12-12 17:59:09 -0600

    finished the react firebase log in. Fully functional, not styled

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,14 +1,16 @@
 import React, { useRef, useState } from 'react'
 import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 export default function Signup() {
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
-    const { signup, currentUser } = useAuth()
+    const { signup } = useAuth()
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
+    let navigate = useNavigate()
 
     async function handleSubmit(e) {
         e.preventDefault()
@@ -21,6 +23,7 @@ export default function Signup() {
             setError("")
             setLoading(true)
             await signup(emailRef.current.value, passwordRef.current.value)
+            navigate('/')
         } catch {
             setError("Failed to create an account")
         }
@@ -34,7 +37,7 @@ export default function Signup() {
             <Card>
                 <Card.Body>
                     <h2 className="text-center mt-2">Sign Up</h2>
-                    {JSON.stringify(currentUser)}
+                    
                     {error && <Alert variant="danger">{error}</Alert>} 
                     <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
@@ -58,7 +61,7 @@ export default function Signup() {
 
             </Card>
             <div className="w-100 text-center mt-2">
-                Already have an account? Log In
+                Already have an account? <NavLink to="/login">Log In Here</NavLink>
             </div>       
         </>
     )
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -11,15 +11,38 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
     const [currentUser, setCurrentUser] = useState()
+    const [loading, setLoading] = useState(true)
 
 
     function signup(email, password) {
         return auth.createUserWithEmailAndPassword(email, password)
     }
 
+    function login(email, password) {
+        return auth.signInWithEmailAndPassword(email, password)
+    }
+
+    function logout () {
+        return auth.signOut()
+    }
+
+    function resetPassword(email) {
+        return auth.sendPasswordResetEmail(email)
+    }
+
+    function updateEmail(email) {
+        return currentUser.updateEmail(email)
+    }
+
+    function updatePassword(password){
+        return currentUser.updatePassword(password)
+    }
+
+
     useEffect(() => {
         const unsubscribe = auth.onAuthStateChanged(user => {
             setCurrentUser(user)
+            setLoading(false)
         })
 
         return unsubscribe
@@ -28,10 +51,17 @@ export function AuthProvider({ children }) {
 
     const value = {
         currentUser,
-        signup 
+        login,
+        signup,
+        logout,
+        resetPassword, 
+        updateEmail,
+        updatePassword
     }
 
     return (
-        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
     )
 }
